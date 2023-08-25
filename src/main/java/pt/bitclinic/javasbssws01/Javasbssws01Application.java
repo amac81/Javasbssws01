18a51e82b2dad9a10a5c154d0ab532f7c97753b4
amac81 <arnaldocanelas@gmail.com>
2023-08-25 16:55:43 +0100

    Spring security with ROLES

--- src/main/java/pt/bitclinic/javasbssws01/Javasbssws01Application.java
+++ src/main/java/pt/bitclinic/javasbssws01/Javasbssws01Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Javasbws01Application {
+public class Javasbssws01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Javasbws01Application.class, args);
+		SpringApplication.run(Javasbssws01Application.class, args);
 	}
 
 }
--- src/main/java/pt/bitclinic/javasbssws01/config/SecurityConfig.java
+++ src/main/java/pt/bitclinic/javasbssws01/config/SecurityConfig.java
@@ -2,34 +2,58 @@ package pt.bitclinic.javasbssws01.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
 	
 	@Bean
-	protected InMemoryUserDetailsManager userDetailsManager() {
-		UserDetails jonh = User.builder()
-				.username("jonh")
-				.password("{noop}test123")
-				.roles("EMPLOYEE")
+	InMemoryUserDetailsManager userDetailsManager() {
+		UserDetails jonh = User.builder().username("jonh").password("{noop}test123").roles("EMPLOYEE").build();
+
+		UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER")
 				.build();
-		
-		UserDetails mary = User.builder()
-				.username("mary")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER")
-				.build();
-		
-		UserDetails susan = User.builder()
-				.username("susan")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER", "ADMIN")
-				.build();
-		
-		return new InMemoryUserDetailsManager(jonh, mary, susan);		
+
+		UserDetails susan = User.builder().username("susan").password("{noop}test123")
+				.roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+
+		return new InMemoryUserDetailsManager(jonh, mary, susan);
 	}
 
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(configurer -> 
+			configurer
+				.requestMatchers(HttpMethod.GET, "/api").permitAll()
+			    .requestMatchers(HttpMethod.GET, "/api/users").hasRole("EMPLOYEE")
+			    .requestMatchers(HttpMethod.GET, "/api/users/**").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.POST, "/api/users").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT, "/api/users/**").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN"));
+
+		// Use HTTP basic authentication
+		http.httpBasic(Customizer.withDefaults());
+
+		// Disable CSRF protection
+		http.csrf(csrf -> csrf.disable());
+		
+		/*
+		 * Cross-Site Request Forgery (CSRF) is an attack that forces authenticated
+		 * users to submit a request to a Web application against which they are 
+		 * currently authenticated. CSRF attacks exploit the trust a Web application has
+		 * in an authenticated user.
+		 *
+		 * In general CSRF protection is not required for stateless REST API's that use
+		 * POST, PUT, DELETE, and/or PATCH.
+		 *
+		 */
+		return http.build();
+	}
 }
